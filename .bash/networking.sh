869aad551a0e6f52c1a11bc59846d96824ec092d
Brandon Scott <brandongregoryscott@gmail.com>
2019-12-25 16:31:42 -0500

    master Added wrapper functions for inspecting logs for ssh authentication and fail2ban

--- .bash/networking.sh
+++ .bash/networking.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function showfailedlogins() {
+    sudo grep sshd.\*Failed /var/log/auth.log | tac | grep -v "grep" | less
+}
+
+function showfailed2ban() {
+    sudo cat /var/log/fail2ban.log | tac | less
+}
+
 function ufwblock() {
     for ip in "$@"
 	do
