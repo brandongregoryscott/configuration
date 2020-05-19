ef4ea9be85abd583b1d29f38a7fef7d088f104b3
Brandon Scott <brandongregoryscott@gmail.com>
2020-05-19 07:34:48 -0400

    Add sqlcmd alias for reference

--- .bash/alias.sh
+++ .bash/alias.sh
@@ -2,3 +2,4 @@
 alias and-cli="npx and-cli"
 alias and-cli-dev="node ~/AndcultureCode.Cli/cli.js"
 alias pagekite="/c/Python27/python.exe /c/Programs/pagekite.py 5001 https://bscott.pagekite.me"
+alias sqlcmd="/opt/mssql-tools/bin/sqlcmd"
