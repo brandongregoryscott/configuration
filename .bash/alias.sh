f86a3029472d1241ae153af22b1386fc5af1cd2e
Brandon Scott <brandongregoryscott@gmail.com>
2020-05-31 13:26:57 -0400

    Add stat alias for mac

--- .bash/alias.sh
+++ .bash/alias.sh
@@ -3,3 +3,7 @@ alias and-cli="npx and-cli"
 alias and-cli-dev="node ~/AndcultureCode.Cli/cli.js"
 alias pagekite="/c/Python27/python.exe /c/Programs/pagekite.py 5001 https://bscott.pagekite.me"
 alias sqlcmd="/opt/mssql-tools/bin/sqlcmd"
+if isMac;
+then
+    alias stat="stat -x"
+fi;
