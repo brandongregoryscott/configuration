c5a80d713e3e01f12b91fb71799bf366581f5710
Brandon Scott <brandongregoryscott@gmail.com>
2020-06-25 08:25:17 -0400

    Update and-cli-dev alias

--- .bash/alias.sh
+++ .bash/alias.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 alias and-cli="npx and-cli"
-alias and-cli-dev="node ~/AndcultureCode.Cli/cli.js"
+alias and-cli-dev="node ~/acc/fork/AndcultureCode.Cli/and-cli.js"
 alias pagekite="/c/Python27/python.exe /c/Programs/pagekite.py 5001 https://bscott.pagekite.me"
 alias sqlcmd="/opt/mssql-tools/bin/sqlcmd"
 if isMac;
